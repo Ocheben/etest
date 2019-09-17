5ef2202e42e079b1647a46260b6290c744a6513f
oche.onoja <oche.onoja@stetis.com>
2019-09-17 19:37:29 +0100

    Final Commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,11 @@
 const toggleDropdown = () => {
     const x = document.getElementById("mobile-nav");
+    const header = document.getElementById("banner")
     if (x.className === "res-nav") {
       x.className += " responsive";
+      header.className += " responsive";
     } else {
       x.className = "res-nav";
+      header.className = "banner"
     }
   }
